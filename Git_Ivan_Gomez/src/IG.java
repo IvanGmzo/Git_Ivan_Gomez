468dcc61c9d210c12adef347115ca52d5e10ef94
Ivan Gomez <ivan.gomez@live.u-tad.com>
2022-11-07 20:27:51 +0100

    Modificación 3

--- Git_Ivan_Gomez/src/IG.java
+++ Git_Ivan_Gomez/src/IG.java
@@ -4,6 +4,7 @@ public class IG {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Alumno: Ivan Gomez");
+		System.out.println("Modificación desde Master - Cambiado");
 	}
 
 }
