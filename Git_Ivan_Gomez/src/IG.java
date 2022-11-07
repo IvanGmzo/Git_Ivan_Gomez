c4c8adf1acaf05607733b2ac0d0f29a3779db465
Ivan Gomez <ivan.gomez@live.u-tad.com>
2022-11-07 20:21:14 +0100

    Revert "Modificación 1"
    This reverts commit 46a95d57d5ba09a39af93fd91c32504966a722df.

--- Git_Ivan_Gomez/src/IG.java
+++ Git_Ivan_Gomez/src/IG.java
@@ -4,8 +4,6 @@ public class IG {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Alumno: Ivan Gomez");
-		System.out.println("Primera Modificación desde Master");
-
 	}
 
 }
