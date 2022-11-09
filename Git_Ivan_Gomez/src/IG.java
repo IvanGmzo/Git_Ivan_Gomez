8b873daeaa5a4098e32a1fb32f9ea138bb70e166
IvanGmzo <115454239+IvanGmzo@users.noreply.github.com>
2022-11-09 17:37:31 +0100

    Modificacion 5

--- Git_Ivan_Gomez/src/IG.java
+++ Git_Ivan_Gomez/src/IG.java
@@ -6,6 +6,7 @@ public class IG {
 		System.out.print("Alumno: Ivan Gomez");
 		System.out.println("Modificación desde Master - Cambiado");
 		System.out.println("Modificación desde Dev");
+		System.out.println("Modificación desde GitHub");
 
 	}
 
